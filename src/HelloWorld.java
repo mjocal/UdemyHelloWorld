5c5b89db8a21227eb76d93c6cef275a7c4193fef
Majo Calderon <majo_cal@hotmail.com>
2019-03-01 10:37:43 -0600

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+
+/**
+ *
+ * @author Majo Calderon
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Puto el que lo lea");
+        
+        int myFirstNumber  = (5 + 8) + (10/5);
+        int mySecondNumber = 12;
+        int myThirdNumber  = mySecondNumber / 6;
+        
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        
+        System.out.println(myFirstNumber);
+        System.out.println("myFirstNumber");
+        System.out.println(myTotal);     
+    }   
+}
